78a5d75bdbcff370245f4503a807771b8a4195ec
Darren Griffith <darren.griffith@wpp.com>
2021-06-29 15:55:17 +0000

    Added vnet module 2

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network-dvg" {
   source  = "iacdev.wpp.cloud/WPP-Lab/network-dvg/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "network"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
